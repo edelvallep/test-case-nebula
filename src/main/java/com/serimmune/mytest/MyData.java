83d45c011db3fdc114382b8180c4c8f6909d69c1
Enrique <edelvallep@gmail.com>
2019-08-14 17:08:45 +0000

    iniciando prueba

---
+++ src/main/java/com/serimmune/mytest/MyData.java
@@ -0,0 +1,12 @@
+package com.serimmune.mytest;
+
+public class MyData {
+
+  public static void main(final String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Git nbula release plugin configured");
+    System.out.println("test");
+    System.out.println("Mytest minor release 1.1.4 build");
+  }
+
+}
